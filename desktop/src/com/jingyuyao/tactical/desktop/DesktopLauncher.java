f899d1c0521542ddb1112abb60aa22344afe85ad
Jingyu Yao <yjy.emails@gmail.com>
2016-12-10 15:21:31 -0500

    Initial build

---
+++ desktop/src/com/jingyuyao/tactical/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jingyuyao.tactical.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jingyuyao.tactical.TacticalAdventure;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TacticalAdventure(), config);
+	}
+}
---
+++ html/src/com/jingyuyao/tactical/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.jingyuyao.tactical.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.jingyuyao.tactical.TacticalAdventure;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new TacticalAdventure();
+        }
+}
