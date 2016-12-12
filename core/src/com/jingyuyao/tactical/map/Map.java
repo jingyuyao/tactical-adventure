fe027a03e3f9085e71e935f51bb9ea55a9bffc1e
Jingyu Yao <yjy.emails@gmail.com>
2016-12-12 13:07:41 -0500

    Oops

--- core/src/com/jingyuyao/tactical/map/Map.java
+++ core/src/com/jingyuyao/tactical/map/Map.java
@@ -1,16 +1,23 @@
 package com.jingyuyao.tactical.map;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.google.common.base.Preconditions;
 
 /**
- * Game representation of {@link TiledMap}
+ * Game representation of {@link TiledMap}.
+ * Map has its own stage with actors receiving touch input.
+ * It is rendered in a grid scale (i.e. showing a 30x20 grid).
  */
 public class Map {
-    private static final String TERRAIN_LAYER = "terrain";
-
+    private final Stage stage;
     private final TiledMap tiledMap;
+    private final OrthogonalTiledMapRenderer mapRenderer;
+
     /**
      * (0,0) starts at bottom left just like {@link TiledMapTileLayer}.
      */
@@ -18,15 +25,37 @@ public class Map {
     private final int height;
     private final int width;
 
-    Map(final TiledMap tiledMap, final Terrain[][] terrainMap)    {
+    Map(final Stage stage, final TiledMap tiledMap, final OrthogonalTiledMapRenderer mapRenderer,
+            final Terrain[][] terrainMap) {
+        this.stage = stage;
         this.tiledMap = tiledMap;
         this.terrainMap = terrainMap;
+        this.mapRenderer = mapRenderer;
         height = terrainMap.length;
         width = terrainMap[0].length;
     }
 
+    public void act(float delta) {
+        stage.act(delta);
+    }
+
+    public void draw() {
+        mapRenderer.setView((OrthographicCamera) stage.getCamera());
+        mapRenderer.render();
+    }
+
     public static class MapFactory {
+        private static final float TILE_SCALE = 1/32f;
+        private static final int MAP_WIDTH = 25; // # tiles
+        private static final int MAP_HEIGHT = 20;
+        private static final String TERRAIN_LAYER = "terrain";
+
         public static Map create(final TiledMap tiledMap) {
+            OrthographicCamera camera = new OrthographicCamera();
+            FitViewport viewport = new FitViewport(MAP_WIDTH, MAP_HEIGHT, camera);
+            Stage stage = new Stage(viewport);
+            OrthogonalTiledMapRenderer mapRenderer = new OrthogonalTiledMapRenderer(tiledMap, TILE_SCALE);
+
             TiledMapTileLayer terrainLayer = (TiledMapTileLayer) tiledMap.getLayers().get(TERRAIN_LAYER);
             Preconditions.checkNotNull(terrainLayer, "Map must contain a terrain layer.");
 
@@ -40,10 +69,11 @@ public class Map {
                 for (int x = 0; x < width; x++) {
                     TiledMapTileLayer.Cell cell = terrainLayer.getCell(x, y);
                     terrainMap[y][x] = Terrain.TerrainFactory.create(cell);
+                    // TODO: Add terrain to stage
                 }
             }
 
-            return new Map(tiledMap, terrainMap);
+            return new Map(stage, tiledMap, mapRenderer, terrainMap);
         }
     }
 }
--- core/src/com/jingyuyao/tactical/screen/GameScreen.java
+++ core/src/com/jingyuyao/tactical/screen/GameScreen.java
@@ -7,24 +7,22 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.jingyuyao.tactical.Assets;
 import com.jingyuyao.tactical.TacticalAdventure;
 import com.jingyuyao.tactical.map.Map;
-import com.jingyuyao.tactical.map.MapRenderer;
 
 public class GameScreen extends ScreenAdapter {
     private final TacticalAdventure game;
     private final Map map;
-    private final MapRenderer mapRenderer;
 
     public GameScreen(TacticalAdventure game) {
         this.game = game;
         final TiledMap tiledMap = game.getAssetManager().get(Assets.TEST_MAP);
         map = Map.MapFactory.create(tiledMap);
-        mapRenderer = new MapRenderer(tiledMap);
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		mapRenderer.render();
+		map.act(delta);
+		map.draw();
     }
 }
