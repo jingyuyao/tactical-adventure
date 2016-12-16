e500f1122e5f988bdcd2f94136390eb318d0d5f8
Jingyu Yao <yjy.emails@gmail.com>
2016-12-16 18:43:33 -0500

    Better desktop size

--- desktop/src/com/jingyuyao/tactical/desktop/DesktopLauncher.java
+++ desktop/src/com/jingyuyao/tactical/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.jingyuyao.tactical.TacticalAdventure;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 25 * 32;
+		config.height = 15 * 32;
 		new LwjglApplication(new TacticalAdventure(), config);
 	}
 }
