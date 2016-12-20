c0d65dd10cfe1ea24ff75011f406a38c11186c49
Jingyu Yao <yjy.emails@gmail.com>
2016-12-19 19:25:28 -0500

    more testing stuff

---
+++ core/test/com/jingyuyao/tactical/SampleTest.java
@@ -0,0 +1,12 @@
+package com.jingyuyao.tactical;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SampleTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
