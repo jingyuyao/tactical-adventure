74586a50f00f9aaba1eb7fe64f8858e9d5fe6c59
Jingyu Yao <yjy.emails@gmail.com>
2016-12-29 19:38:41 -0500

    cleaned up actor module a bit

--- core/src/com/jingyuyao/tactical/view/ViewModule.java
+++ core/src/com/jingyuyao/tactical/view/ViewModule.java
@@ -1,10 +1,13 @@
 package com.jingyuyao.tactical.view;
 
 import com.google.inject.AbstractModule;
+import com.jingyuyao.tactical.view.actor.ActorModule;
 
 public class ViewModule extends AbstractModule {
     @Override
     protected void configure() {
+        install(new ActorModule());
+
         bind(LevelScreenFactory.class);
         bind(MapViewFactory.class);
         bind(MapUIFactory.class);
--- core/src/com/jingyuyao/tactical/view/actor/ActorFactory.java
+++ core/src/com/jingyuyao/tactical/view/actor/ActorFactory.java
@@ -1,69 +1,48 @@
 package com.jingyuyao.tactical.view.actor;
 
-import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.google.common.eventbus.EventBus;
-import com.jingyuyao.tactical.AssetModule;
 import com.jingyuyao.tactical.controller.MapActorController;
-import com.jingyuyao.tactical.model.Level;
+import com.jingyuyao.tactical.model.Highlighter;
 import com.jingyuyao.tactical.model.Marker;
+import com.jingyuyao.tactical.model.Waiter;
 import com.jingyuyao.tactical.model.object.Enemy;
 import com.jingyuyao.tactical.model.object.Player;
 import com.jingyuyao.tactical.model.object.Terrain;
+import com.jingyuyao.tactical.model.state.MapState;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.HashMap;
 
 /**
  * Creates {@link BaseActor} from models and adds the proper controllers.
  */
 @Singleton
 public class ActorFactory {
-    public static final float ACTOR_SIZE = 1f; // world units
+    private static final float ACTOR_SIZE = 1f; // world units
 
     private final EventBus eventBus;
-    private final AssetManager assetManager;
-    private final Level level;
+    private final Waiter waiter;
+    private final MapState mapState;
+    private final Highlighter highlighter;
     private final java.util.Map<Marker, Sprite> markerSpriteMap;
-    private final java.util.Map<Class, Color> typeColorMap;
+    private final java.util.Map<String, Sprite> characterSpriteMap;
 
     @Inject
-    public ActorFactory(EventBus eventBus, AssetManager assetManager, Level level) {
+    public ActorFactory(
+            EventBus eventBus,
+            Waiter waiter, MapState mapState,
+            Highlighter highlighter,
+            java.util.Map<Marker, Sprite> markerSpriteMap,
+            java.util.Map<String, Sprite> characterSpriteMap) {
         this.eventBus = eventBus;
-        this.assetManager = assetManager;
-        this.level = level;
-        markerSpriteMap = new HashMap<Marker, Sprite>();
-        markerSpriteMap.put(
-                Marker.HIGHLIGHT,
-                new Sprite(assetManager.get(AssetModule.HIGHLIGHT, Texture.class))
-        );
-        markerSpriteMap.put(
-                Marker.CAN_MOVE_TO,
-                new Sprite(assetManager.get(AssetModule.MOVE, Texture.class))
-        );
-        markerSpriteMap.put(
-                Marker.DANGER,
-                new Sprite(assetManager.get(AssetModule.DANGER, Texture.class))
-        );
-        markerSpriteMap.put(
-                Marker.CAN_ATTACK,
-                new Sprite(assetManager.get(AssetModule.ATTACK, Texture.class))
-        );
-        markerSpriteMap.put(
-                Marker.POTENTIAL_TARGET,
-                new Sprite(assetManager.get(AssetModule.POTENTIAL_TARGET, Texture.class))
-        );
-        markerSpriteMap.put(
-                Marker.CHOSEN_TARGET,
-                new Sprite(assetManager.get(AssetModule.CHOSEN_TARGET, Texture.class))
-        );
-        typeColorMap = new HashMap<Class, Color>();
-        typeColorMap.put(Player.class, Color.WHITE);
-        typeColorMap.put(Enemy.class, Color.RED);
+        this.waiter = waiter;
+        this.mapState = mapState;
+        this.highlighter = highlighter;
+        this.markerSpriteMap = markerSpriteMap;
+        this.characterSpriteMap = characterSpriteMap;
     }
 
     public Actor create(Player player) {
@@ -71,11 +50,11 @@ public class ActorFactory {
                 eventBus,
                 player,
                 ACTOR_SIZE,
-                level.getWaiter(),
+                waiter,
                 markerSpriteMap,
-                new Sprite(assetManager.get("sprites/" + player.getName() + ".png", Texture.class)),
-                typeColorMap.get(player.getClass()),
-                new MapActorController(level.getMapState(), level.getHighlighter(), player, ACTOR_SIZE)
+                characterSpriteMap.get(player.getName()),
+                Color.WHITE,
+                new MapActorController(mapState, highlighter, player, ACTOR_SIZE)
         );
     }
 
@@ -84,11 +63,11 @@ public class ActorFactory {
                 eventBus,
                 enemy,
                 ACTOR_SIZE,
-                level.getWaiter(),
+                waiter,
                 markerSpriteMap,
-                new Sprite(assetManager.get("sprites/" + enemy.getName() + ".png", Texture.class)),
-                typeColorMap.get(enemy.getClass()),
-                new MapActorController(level.getMapState(), level.getHighlighter(), enemy, ACTOR_SIZE)
+                characterSpriteMap.get(enemy.getName()),
+                Color.RED,
+                new MapActorController(mapState, highlighter, enemy, ACTOR_SIZE)
         );
     }
 
@@ -97,9 +76,9 @@ public class ActorFactory {
                 eventBus,
                 terrain,
                 ACTOR_SIZE,
-                level.getWaiter(),
+                waiter,
                 markerSpriteMap,
-                new MapActorController(level.getMapState(), level.getHighlighter(), terrain, ACTOR_SIZE)
+                new MapActorController(mapState, highlighter, terrain, ACTOR_SIZE)
         );
     }
 }
