dadf26e23d6f348fd8c50fdcf1bbedb3256c8a01
Jingyu Yao <yjy.emails@gmail.com>
2017-01-02 20:26:59 -0500

    moved model event to common

--- core/src/com/jingyuyao/tactical/model/event/HideAttackPlan.java
+++ core/src/com/jingyuyao/tactical/model/event/HideAttackPlan.java
@@ -1,4 +1,6 @@
 package com.jingyuyao.tactical.model.event;
 
+import com.jingyuyao.tactical.model.common.ModelEvent;
+
 public class HideAttackPlan implements ModelEvent {
 }
--- core/src/com/jingyuyao/tactical/model/event/WaitChange.java
+++ core/src/com/jingyuyao/tactical/model/event/WaitChange.java
@@ -1,5 +1,7 @@
 package com.jingyuyao.tactical.model.event;
 
+import com.jingyuyao.tactical.model.common.ModelEvent;
+
 public class WaitChange implements ModelEvent {
     private final boolean waiting;
 
