5958617e815ce1c2577f57e7cf916bd10392bc7e
Jingyu Yao <yjy.emails@gmail.com>
2017-01-06 17:08:43 -0500

    tests for mark module
    not sure what i accomplishes though...

---
+++ core/test/com/jingyuyao/tactical/model/mark/MarkModuleTest.java
@@ -0,0 +1,33 @@
+package com.jingyuyao.tactical.model.mark;
+
+import com.google.inject.Guice;
+import com.google.inject.testing.fieldbinder.Bind;
+import com.google.inject.testing.fieldbinder.BoundFieldModule;
+import com.jingyuyao.tactical.model.Waiter;
+import com.jingyuyao.tactical.model.map.Targets;
+import com.jingyuyao.tactical.model.map.Terrains;
+import javax.inject.Inject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MarkModuleTest {
+
+  @Bind
+  @Mock
+  private Waiter waiter;
+  @Bind
+  @Mock
+  private Terrains terrains;
+  @Mock
+  private Targets targets;
+  @Inject
+  private MarkingFactory markingFactory;
+
+  @Test
+  public void can_create_module() {
+    Guice.createInjector(BoundFieldModule.of(this), new MarkModule()).injectMembers(this);
+  }
+}
