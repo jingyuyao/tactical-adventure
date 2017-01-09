cd8322d2d8b9590a2749e73d1a602a7d281c4fbd
Jingyu Yao <yjy.emails@gmail.com>
2017-01-09 14:14:57 -0500

    added test for module

---
+++ core/test/com/jingyuyao/tactical/model/map/MapModuleTest.java
@@ -0,0 +1,50 @@
+package com.jingyuyao.tactical.model.map;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.eventbus.EventBus;
+import com.google.inject.Guice;
+import com.google.inject.testing.fieldbinder.Bind;
+import com.google.inject.testing.fieldbinder.BoundFieldModule;
+import com.jingyuyao.tactical.model.common.Coordinate;
+import com.jingyuyao.tactical.model.map.Terrain.Type;
+import javax.inject.Inject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MapModuleTest {
+
+  @Bind
+  @Mock
+  private EventBus eventBus;
+  @Inject
+  private Characters characters;
+  @Inject
+  private Characters characters2;
+  @Inject
+  private Terrains terrains;
+  @Inject
+  private Terrains terrains2;
+  @Inject
+  private TerrainFactory terrainFactory;
+
+  @Before
+  public void setUp() {
+    Guice.createInjector(BoundFieldModule.of(this), new MapModule()).injectMembers(this);
+  }
+
+  @Test
+  public void can_create_terrains() {
+    terrainFactory.create(new Coordinate(0, 0), Type.NORMAL);
+  }
+
+  @Test
+  public void singletons() {
+    assertThat(characters).isSameAs(characters2);
+    assertThat(terrains).isSameAs(terrains2);
+  }
+}
