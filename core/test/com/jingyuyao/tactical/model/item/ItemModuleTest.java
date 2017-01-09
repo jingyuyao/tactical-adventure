f00cbcd73874e8891afbca6d5bb2fc9ed2f8a293
Jingyu Yao <yjy.emails@gmail.com>
2017-01-09 15:18:04 -0500

    tests for item module

---
+++ core/test/com/jingyuyao/tactical/model/item/ItemModuleTest.java
@@ -0,0 +1,34 @@
+package com.jingyuyao.tactical.model.item;
+
+import com.google.common.eventbus.EventBus;
+import com.google.inject.Guice;
+import com.google.inject.testing.fieldbinder.Bind;
+import com.google.inject.testing.fieldbinder.BoundFieldModule;
+import java.util.Collections;
+import javax.inject.Inject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ItemModuleTest {
+
+  @Bind
+  @Mock
+  private EventBus eventBus;
+  @Inject
+  private ItemFactory itemFactory;
+
+  @Before
+  public void setUp() {
+    Guice.createInjector(BoundFieldModule.of(this), new ItemModule()).injectMembers(this);
+  }
+
+  @Test
+  public void item_factory() {
+    itemFactory.createHeal("hello", 2);
+    itemFactory.createWeapon("nihao", 1, 1, Collections.<Integer>emptySet());
+  }
+}
