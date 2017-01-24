e1bd8b4972c20c0104fdb51065934e61cc96276d
Jingyu Yao <yjy.emails@gmail.com>
2017-01-23 22:54:36 -0500

    damages after animation

--- core/src/com/jingyuyao/tactical/model/item/AbstractWeapon.java
+++ core/src/com/jingyuyao/tactical/model/item/AbstractWeapon.java
@@ -1,6 +1,8 @@
 package com.jingyuyao.tactical.model.item;
 
 import com.google.common.eventbus.EventBus;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
 import com.jingyuyao.tactical.model.character.Character;
@@ -17,12 +19,22 @@ abstract class AbstractWeapon<T extends WeaponStats> extends BaseItem<T> impleme
 
   @Override
   public ListenableFuture<Void> attack(Character attacker, final Target target) {
-    for (Character opponent : target.getTargetCharacters()) {
-      opponent.damageBy(getItemStats().getAttackPower());
-    }
-    useOnce();
     SettableFuture<Void> future = SettableFuture.create();
     getEventBus().post(new Attack(attacker, future, target));
+    Futures.addCallback(future, new FutureCallback<Void>() {
+      @Override
+      public void onSuccess(Void result) {
+        for (Character opponent : target.getTargetCharacters()) {
+          opponent.damageBy(getItemStats().getAttackPower());
+        }
+        useOnce();
+      }
+
+      @Override
+      public void onFailure(Throwable t) {
+
+      }
+    });
     return future;
   }
 }
