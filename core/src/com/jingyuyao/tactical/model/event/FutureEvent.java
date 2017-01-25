b3974f6b03f93e08cba2c1795cc5a285a7ee202b
Jingyu Yao <yjy.emails@gmail.com>
2017-01-25 15:41:00 -0500

    better base event name

--- core/src/com/jingyuyao/tactical/model/event/FutureEvent.java
+++ core/src/com/jingyuyao/tactical/model/event/FutureEvent.java
@@ -4,12 +4,14 @@ import com.google.common.util.concurrent.SettableFuture;
 
 /**
  * An event that must be completed externally (usually by the view).
- * See {@link ObjectEvent} as to why this class is abstract.
  */
 public class FutureEvent<T> extends ObjectEvent<T> {
 
   private final SettableFuture<Void> future;
 
+  /**
+   * See {@link ObjectEvent#ObjectEvent(Object)} to why this is protected.
+   */
   protected FutureEvent(T object, SettableFuture<Void> future) {
     super(object);
     this.future = future;
