6899e4391c249fc710918646a7eb1d8c3347e59d
Jingyu Yao <yjy.emails@gmail.com>
2017-01-28 14:22:11 -0500

    loader refactor

--- core/src/com/jingyuyao/tactical/data/MapLoader.java
+++ core/src/com/jingyuyao/tactical/data/MapLoader.java
@@ -7,15 +7,9 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableList;
 import com.google.gson.Gson;
 import com.jingyuyao.tactical.model.Model;
-import com.jingyuyao.tactical.model.character.CharacterFactory;
-import com.jingyuyao.tactical.model.character.Enemy;
-import com.jingyuyao.tactical.model.character.PassiveEnemy;
-import com.jingyuyao.tactical.model.item.Item;
 import com.jingyuyao.tactical.model.state.Waiting;
-import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.inject.Singleton;
@@ -27,33 +21,31 @@ public class MapLoader {
 
   private final Model model;
   private final Provider<Waiting> waitingProvider;
-  private final CharacterFactory characterFactory;
   private final Gson gson;
   private final AssetManager assetManager;
   private final OrthogonalTiledMapRenderer mapRenderer;
   private final TerrainLoader terrainLoader;
-  private final ItemLoader itemLoader;
   private final PlayerLoader playerLoader;
+  private final EnemyLoader enemyLoader;
 
   @Inject
   MapLoader(
       Model model,
       Provider<Waiting> waitingProvider,
-      CharacterFactory characterFactory,
       Gson gson,
       AssetManager assetManager,
       OrthogonalTiledMapRenderer mapRenderer,
       TerrainLoader terrainLoader,
-      ItemLoader itemLoader, PlayerLoader playerLoader) {
+      PlayerLoader playerLoader,
+      EnemyLoader enemyLoader) {
     this.model = model;
     this.waitingProvider = waitingProvider;
-    this.characterFactory = characterFactory;
     this.gson = gson;
     this.assetManager = assetManager;
     this.mapRenderer = mapRenderer;
     this.terrainLoader = terrainLoader;
-    this.itemLoader = itemLoader;
     this.playerLoader = playerLoader;
+    this.enemyLoader = enemyLoader;
   }
 
   public void loadMap(String mapName) {
@@ -77,28 +69,9 @@ public class MapLoader {
         height,
         terrainLoader.createTerrains(terrainLayer, width, height),
         playerLoader.createPlayers(mapSave.getPlayers()),
-        createEnemies(mapSave.getEnemies()),
+        enemyLoader.createEnemies(mapSave.getEnemies()),
         waitingProvider.get());
     mapRenderer.setMap(tiledMap);
   }
 
-  private Iterable<Enemy> createEnemies(List<EnemySave> enemySaves) {
-    ImmutableList.Builder<Enemy> builder = ImmutableList.builder();
-    for (EnemySave enemySave : enemySaves) {
-      builder.add(createEnemy(enemySave));
-    }
-    return builder.build();
-  }
-
-  private Enemy createEnemy(EnemySave enemySave) {
-    String className = enemySave.getClassName();
-    List<Item> items = itemLoader.createItems(enemySave.getItems());
-    Enemy enemy;
-    if (PassiveEnemy.class.getSimpleName().equals(className)) {
-      enemy = characterFactory.createPassiveEnemy(enemySave.getData(), items);
-    } else {
-      throw new IllegalArgumentException("Unknown enemy class name: " + className);
-    }
-    return enemy;
-  }
 }
