f713ea5bb7defcddb236386095da5c44aaa294e9
Jingyu Yao <yjy.emails@gmail.com>
2017-02-09 16:26:23 -0500

    added using consumable state

---
+++ core/src/com/jingyuyao/tactical/model/state/UsingConsumable.java
@@ -0,0 +1,37 @@
+package com.jingyuyao.tactical.model.state;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.eventbus.EventBus;
+import com.google.inject.assistedinject.Assisted;
+import com.jingyuyao.tactical.model.ModelModule.ModelEventBus;
+import com.jingyuyao.tactical.model.character.Player;
+import com.jingyuyao.tactical.model.item.Consumable;
+import javax.inject.Inject;
+
+public class UsingConsumable extends BasePlayerState {
+
+  private final Consumable consumable;
+
+  @Inject
+  UsingConsumable(
+      @ModelEventBus EventBus eventBus,
+      MapState mapState,
+      StateFactory stateFactory,
+      @Assisted Player player,
+      @Assisted Consumable consumable) {
+    super(eventBus, mapState, stateFactory, player);
+    this.consumable = consumable;
+  }
+
+  @Override
+  public ImmutableList<Action> getActions() {
+    return ImmutableList.of(
+        new UseConsumableAction(this, getPlayer(), consumable),
+        new BackAction(this)
+    );
+  }
+
+  public Consumable getConsumable() {
+    return consumable;
+  }
+}
---
+++ core/test/com/jingyuyao/tactical/model/state/UsingConsumableTest.java
@@ -0,0 +1,67 @@
+package com.jingyuyao.tactical.model.state;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.verify;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.eventbus.EventBus;
+import com.jingyuyao.tactical.TestHelpers;
+import com.jingyuyao.tactical.model.character.Player;
+import com.jingyuyao.tactical.model.event.ExitState;
+import com.jingyuyao.tactical.model.item.Consumable;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UsingConsumableTest {
+
+  @Mock
+  private EventBus eventBus;
+  @Mock
+  private MapState mapState;
+  @Mock
+  private StateFactory stateFactory;
+  @Mock
+  private Player player;
+  @Mock
+  private Consumable consumable;
+  @Captor
+  private ArgumentCaptor<Object> argumentCaptor;
+
+  private UsingConsumable usingConsumable;
+
+  @Before
+  public void setUp() {
+    usingConsumable = new UsingConsumable(eventBus, mapState, stateFactory, player, consumable);
+  }
+
+  @Test
+  public void enter() {
+    usingConsumable.enter();
+
+    verify(eventBus).post(argumentCaptor.capture());
+    assertThat(argumentCaptor.getValue()).isSameAs(usingConsumable);
+  }
+
+  @Test
+  public void exit() {
+    usingConsumable.exit();
+
+    verify(eventBus).post(argumentCaptor.capture());
+    TestHelpers.verifyObjectEvent(argumentCaptor, 0, usingConsumable, ExitState.class);
+  }
+
+  @Test
+  public void actions() {
+    ImmutableList<Action> actions = usingConsumable.getActions();
+
+    assertThat(actions).hasSize(2);
+    assertThat(actions.get(0)).isInstanceOf(UseConsumableAction.class);
+    assertThat(actions.get(1)).isInstanceOf(BackAction.class);
+  }
+}
