67fc5f3cf81ebcd5f1a37c2763022b96752faba1
Jingyu Yao <yjy.emails@gmail.com>
2017-02-10 18:03:01 -0500

    document the new action paradigm

--- core/src/com/jingyuyao/tactical/model/state/Action.java
+++ core/src/com/jingyuyao/tactical/model/state/Action.java
@@ -4,5 +4,9 @@ public interface Action {
 
   String getName();
 
+  /**
+   * Perform the action. <br/> Implementation should be simple method references. All complexity
+   * should be contained in the {@link State} this action originate from.
+   */
   void run();
 }
