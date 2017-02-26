83313b8a2a553a0d9975f481d7fb2b51d12e79fc
Jingyu Yao <yjy.emails@gmail.com>
2017-02-25 19:27:14 -0500

    world screen test

---
+++ core/test/com/jingyuyao/tactical/view/WorldScreenTest.java
@@ -0,0 +1,88 @@
+package com.jingyuyao.tactical.view;
+
+import static org.mockito.Mockito.verify;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.jingyuyao.tactical.controller.WorldController;
+import com.jingyuyao.tactical.view.marking.Markings;
+import com.jingyuyao.tactical.view.ui.UI;
+import com.jingyuyao.tactical.view.world.World;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class WorldScreenTest {
+
+  @Mock
+  private Batch batch;
+  @Mock
+  private World world;
+  @Mock
+  private Markings markings;
+  @Mock
+  private UI ui;
+  @Mock
+  private WorldController worldController;
+  @Mock
+  private GL20 gl20;
+
+  private WorldScreen worldScreen;
+
+  @Before
+  public void setUp() {
+    worldScreen = new WorldScreen(batch, world, markings, ui, worldController);
+    Gdx.gl = gl20;
+  }
+
+  @Test
+  public void show() {
+    worldScreen.show();
+
+    verify(worldController).receiveInput();
+  }
+
+  @Test
+  public void hide() {
+    worldScreen.hide();
+
+    verify(worldController).stopReceivingInput();
+  }
+
+  @Test
+  public void render() {
+    worldScreen.render(10f);
+
+    InOrder inOrder = Mockito.inOrder(world, markings, ui, gl20);
+    inOrder.verify(ui).act(10f);
+    inOrder.verify(markings).act(10f);
+    inOrder.verify(world).act(10f);
+    inOrder.verify(gl20).glClear(GL20.GL_COLOR_BUFFER_BIT);
+    inOrder.verify(world).draw();
+    inOrder.verify(markings).draw();
+    inOrder.verify(ui).draw();
+  }
+
+  @Test
+  public void resize() {
+    worldScreen.resize(20, 30);
+
+    verify(world).resize(20, 30);
+    verify(ui).resize(20, 30);
+  }
+
+  @Test
+  public void dispose() {
+    worldScreen.dispose();
+
+    verify(world).dispose();
+    verify(ui).dispose();
+    verify(batch).dispose();
+  }
+}
