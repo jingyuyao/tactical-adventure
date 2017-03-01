0d00b7cb0c42df1bfbd824ecbdbbfde75928897e
Jingyu Yao <yjy.emails@gmail.com>
2017-03-01 12:10:45 -0500

    removed unused code

--- core/src/com/jingyuyao/tactical/view/util/ViewUtil.java
+++ core/src/com/jingyuyao/tactical/view/util/ViewUtil.java
@@ -1,19 +1,10 @@
 package com.jingyuyao.tactical.view.util;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class ViewUtil {
-
-  /**
-   * Draw {@link Sprite} using {@link Batch} with dimension from {@link Actor}.
-   */
-  public static void draw(Batch batch, Sprite sprite, Actor actor) {
-    sprite.setBounds(actor.getX(), actor.getY(), actor.getWidth(), actor.getHeight());
-    sprite.draw(batch);
-  }
 
   /**
    * Draw {@link TextureRegion} using {@link Batch} with dimension from {@link Actor}.
