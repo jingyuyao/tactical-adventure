064f55065d563cbbc90b9f5d4d25b5435de94c61
Jingyu Yao <yjy.emails@gmail.com>
2017-03-23 17:44:51 -0400

    controller now uses world

--- core/src/com/jingyuyao/tactical/controller/ControllerModule.java
+++ core/src/com/jingyuyao/tactical/controller/ControllerModule.java
@@ -2,7 +2,7 @@ package com.jingyuyao.tactical.controller;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
-import com.jingyuyao.tactical.model.map.Terrains;
+import com.jingyuyao.tactical.model.World;
 import com.jingyuyao.tactical.model.state.SelectionHandler;
 
 public class ControllerModule extends AbstractModule {
@@ -10,7 +10,7 @@ public class ControllerModule extends AbstractModule {
   @Override
   protected void configure() {
     requireBinding(SelectionHandler.class);
-    requireBinding(Terrains.class);
+    requireBinding(World.class);
 
     install(new FactoryModuleBuilder().build(ControllerFactory.class));
   }
