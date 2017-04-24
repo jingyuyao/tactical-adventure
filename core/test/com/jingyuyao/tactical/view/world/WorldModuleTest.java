1d409c69553da78a482358024f86dafc97929ece
Jingyu Yao <yjy.emails@gmail.com>
2017-04-24 14:52:43 -0400

    removed an unnecessary headless environment loading

--- core/test/com/jingyuyao/tactical/view/world/WorldModuleTest.java
+++ core/test/com/jingyuyao/tactical/view/world/WorldModuleTest.java
@@ -1,6 +1,5 @@
 package com.jingyuyao.tactical.view.world;
 
-import com.badlogic.gdx.backends.headless.HeadlessNativesLoader;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.testing.fieldbinder.Bind;
@@ -8,7 +7,6 @@ import com.google.inject.testing.fieldbinder.BoundFieldModule;
 import com.jingyuyao.tactical.MockGameModule;
 import com.jingyuyao.tactical.controller.WorldCamera;
 import com.jingyuyao.tactical.controller.WorldController;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -26,11 +24,6 @@ public class WorldModuleTest {
 
   @Inject
   private WorldView worldView;
-
-  @BeforeClass
-  public static void setUpClass() {
-    HeadlessNativesLoader.load();
-  }
 
   @Test
   public void can_create_module() {
