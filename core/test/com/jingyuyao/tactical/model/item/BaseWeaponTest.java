97129e0aff2c2b30e25ab31bab48e46ca5aea6e3
Jingyu Yao <yjy.emails@gmail.com>
2017-05-04 17:56:16 -0400

    add life steal and recoil effect that can be enable by all weapons

--- core/test/com/jingyuyao/tactical/model/item/BaseWeaponTest.java
+++ core/test/com/jingyuyao/tactical/model/item/BaseWeaponTest.java
@@ -1,14 +1,13 @@
 package com.jingyuyao.tactical.model.item;
 
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 import com.google.common.base.Optional;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.jingyuyao.tactical.model.character.Character;
 import com.jingyuyao.tactical.model.world.Cell;
-import com.jingyuyao.tactical.model.world.Movements;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,7 +15,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
-public class AbstractWeaponTest {
+public class BaseWeaponTest {
 
   @Mock
   private Target target;
@@ -31,29 +30,44 @@ public class AbstractWeaponTest {
   @Mock
   private Character character2;
 
-  private AbstractWeapon baseWeapon;
+  private BaseWeapon baseWeapon;
 
   @Before
   public void setUp() {
-    baseWeapon = new AbstractWeapon(10) {
-      @Override
-      public ImmutableList<Target> createTargets(Movements movements, Cell from) {
-        throw new UnsupportedOperationException();
-      }
-    };
-  }
-
-  @Test
-  public void damages() {
     when(target.getTargetCells()).thenReturn(ImmutableSet.of(cell1, cell2));
     when(cell1.character()).thenReturn(Optional.of(character1));
     when(cell2.character()).thenReturn(Optional.of(character2));
     when(character1.getDefense()).thenReturn(3);
     when(character2.getDefense()).thenReturn(100);
+  }
 
+  @Test
+  public void apply_basic() {
+    baseWeapon = new BaseWeapon(10, false, 0, false, 0);
     baseWeapon.apply(attacker, target);
 
     verify(character1).damageBy(7);
     verify(character2).damageBy(0);
+    verifyZeroInteractions(attacker);
+  }
+
+  @Test
+  public void apply_life_steal() {
+    baseWeapon = new BaseWeapon(10, true, 0.5f, false, 0);
+    baseWeapon.apply(attacker, target);
+
+    verify(character1).damageBy(7);
+    verify(character2).damageBy(0);
+    verify(attacker).healBy(3);
+  }
+
+  @Test
+  public void apply_recoil() {
+    baseWeapon = new BaseWeapon(10, false, 0, true, 0.5f);
+    baseWeapon.apply(attacker, target);
+
+    verify(character1).damageBy(7);
+    verify(character2).damageBy(0);
+    verify(attacker).damageBy(3);
   }
 }
