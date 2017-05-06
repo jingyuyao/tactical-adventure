4bf83a516a9dd6cdbc4e0c2b1066cca788529b05
Jingyu Yao <yjy.emails@gmail.com>
2017-05-06 17:59:30 -0400

    model messages in the model folder

--- core/src/com/jingyuyao/tactical/model/i18n/ModelBundle.java
+++ core/src/com/jingyuyao/tactical/model/i18n/ModelBundle.java
@@ -5,10 +5,10 @@ package com.jingyuyao.tactical.model.i18n;
  */
 public class ModelBundle {
 
-  public static final MessageBundle ACTION = new MessageBundle("i18n/game/action/Action");
+  public static final MessageBundle ACTION = new MessageBundle("i18n/model/action/Action");
   public static final MessageBundle CHARACTER_NAME =
-      new MessageBundle("i18n/game/character/CharacterName");
-  public static final MessageBundle ITEM_NAME = new MessageBundle("i18n/game/item/ItemName");
+      new MessageBundle("i18n/model/character/CharacterName");
+  public static final MessageBundle ITEM_NAME = new MessageBundle("i18n/model/item/ItemName");
   public static final MessageBundle ITEM_DESCRIPTION =
-      new MessageBundle("i18n/game/item/ItemDescription");
+      new MessageBundle("i18n/model/item/ItemDescription");
 }
