1552433b109fbe795bd1226c922b2c7e484e3227
Jingyu Yao <yjy.emails@gmail.com>
2017-05-05 22:36:11 -0400

    added i18n representation in the model

---
+++ core/src/com/jingyuyao/tactical/model/i18n/Message.java
@@ -0,0 +1,36 @@
+package com.jingyuyao.tactical.model.i18n;
+
+/**
+ * A reference to an i18n message.
+ */
+public class Message {
+
+  private final MessageBundle bundle;
+  private final String key;
+  private final Object[] args;
+
+  /**
+   * See {@link MessageBundle#create(String, Object...)}.
+   *
+   * @param bundle the {@link MessageBundle} this message belongs to
+   * @param key the key to get the message from the bundle
+   * @param args used to format the returned message, see {@link java.text.MessageFormat}
+   */
+  Message(MessageBundle bundle, String key, Object... args) {
+    this.bundle = bundle;
+    this.key = key;
+    this.args = args;
+  }
+
+  public MessageBundle getBundle() {
+    return bundle;
+  }
+
+  public String getKey() {
+    return key;
+  }
+
+  public Object[] getArgs() {
+    return args;
+  }
+}
---
+++ core/src/com/jingyuyao/tactical/model/i18n/MessageBundle.java
@@ -0,0 +1,29 @@
+package com.jingyuyao.tactical.model.i18n;
+
+/**
+ * Contains the list of messages bundles available. Bundles should be placed within the assets
+ * folder. This class is used instead of {@link java.util.ResourceBundle} so the view can handle
+ * actually getting the messages. We don't want the model to be aware of or access any resources
+ * since doing so would rely on game engine specific code. This means the view should test all the
+ * resources required by the model exists.
+ */
+public enum MessageBundle {
+  ACTION("i18n/game/action/Action.properties");
+
+  private final String path;
+
+  MessageBundle(String path) {
+    this.path = path;
+  }
+
+  public String getPath() {
+    return path;
+  }
+
+  /**
+   * Create a {@link Message}.
+   */
+  public Message create(String key, Object... args) {
+    return new Message(this, key, args);
+  }
+}
