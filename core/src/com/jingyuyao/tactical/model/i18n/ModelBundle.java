330d53866a0dc5259dd573889a16f827592f0a11
Jingyu Yao <yjy.emails@gmail.com>
2017-05-06 23:22:49 -0400

    uniform i18n directory structure

--- core/src/com/jingyuyao/tactical/model/i18n/ModelBundle.java
+++ core/src/com/jingyuyao/tactical/model/i18n/ModelBundle.java
@@ -5,7 +5,7 @@ package com.jingyuyao.tactical.model.i18n;
  */
 public class ModelBundle {
 
-  public static final MessageBundle ACTION = new MessageBundle("i18n/model/action/Action");
+  public static final MessageBundle ACTION = new MessageBundle("i18n/model/state/Action");
   public static final MessageBundle CHARACTER_NAME =
       new MessageBundle("i18n/model/character/CharacterName");
   public static final MessageBundle ITEM_NAME = new MessageBundle("i18n/model/item/ItemName");
