5167921064cec9d2d9d3e6eabbf596cfd4e95477
Jingyu Yao <yjy.emails@gmail.com>
2017-05-18 17:12:35 -0400

    added per level keep alive list

--- core/src/com/jingyuyao/tactical/data/LevelScript.java
+++ core/src/com/jingyuyao/tactical/data/LevelScript.java
@@ -2,14 +2,21 @@ package com.jingyuyao.tactical.data;
 
 import com.jingyuyao.tactical.model.script.LevelTrigger;
 import com.jingyuyao.tactical.model.state.Turn;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 class LevelScript {
 
   private Map<Turn, LevelTrigger> turnTriggers = new HashMap<>();
+  private List<String> keepAlive = new ArrayList<>();
 
   Map<Turn, LevelTrigger> getTurnTriggers() {
     return turnTriggers;
+  }
+
+  List<String> getKeepAlive() {
+    return keepAlive;
   }
 }
