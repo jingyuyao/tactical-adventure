f23b09af3b2b3345cef3043cb9f0e4632ba7604a
Jingyu Yao <yjy.emails@gmail.com>
2018-03-18 17:54:45 -0400

    Clarified what reset does

--- core/src/com/jingyuyao/tactical/menu/MenuBundle.java
+++ core/src/com/jingyuyao/tactical/menu/MenuBundle.java
@@ -8,6 +8,7 @@ class MenuBundle {
   private static final KeyBundle BUNDLE = KeyBundle.i18n("menu/Menu");
   static final StringKey PLAY_BTN = BUNDLE.get("playBtn");
   static final StringKey RESET_BTN = BUNDLE.get("resetBtn");
+  static final StringKey CLEAR_SAVE = BUNDLE.get("clearSave");
   static final StringKey LEVEL_INFO = BUNDLE.get("levelInfo");
   static final StringKey HAS_PROGRESS = BUNDLE.get("hasProgress");
   static final StringKey NO_PROGRESS = BUNDLE.get("noProgress");
